8537d704294fee86f3601ab4c1743f1a79a53045
Natsuneko <10832834+mika-f@users.noreply.github.com>
2021-12-15 21:53:19 +0900

    chore: enable analyzer tracking

---
+++ src/Core/Extensions/VersionExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Extensions;
+
+public static class VersionExtension
+{
+    public static bool IsGreaterThan(this Version obj1, Version obj2)
+    {
+        return obj1.CompareTo(obj2) > 0;
+    }
+
+    public static bool IsGreaterThanOrEquals(this Version obj1, Version obj2)
+    {
+        return obj1.CompareTo(obj2) > 0;
+    }
+
+    public static bool IsSame(this Version obj1, Version obj2)
+    {
+        return obj1.CompareTo(obj2) == 0;
+    }
+
+    public static bool IsLessThan(this Version obj1, Version obj2)
+    {
+        return obj1.CompareTo(obj2) < 0;
+    }
+
+    public static bool IsLessThanOrEquals(this Version obj1, Version obj2)
+    {
+        return obj1.CompareTo(obj2) <= 0;
+    }
+}
