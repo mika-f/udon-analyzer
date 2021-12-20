8cd9316b4a1e16ecce30bf471fa097c35154000b
Natsuneko <10832834+mika-f@users.noreply.github.com>
2021-12-20 23:09:20 +0900

    feat(core): add version range attributes for Udon and UdonSharp

---
+++ src/Core/Attributes/RequireUdonSharpCompilerVersionAttribute.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class RequireUdonSharpCompilerVersionAttribute : Attribute
+{
+    public string VersionRange { get; }
+
+    public RequireUdonSharpCompilerVersionAttribute(string versionRange)
+    {
+        VersionRange = versionRange;
+    }
+}
---
+++ src/Core/Attributes/RequireUdonVersionAttribute.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class RequireUdonVersionAttribute : Attribute
+{
+    public string VersionRange { get; }
+
+    public RequireUdonVersionAttribute(string versionRange)
+    {
+        VersionRange = versionRange;
+    }
+}
