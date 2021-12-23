675d39a71817770f978241544d66908504880d6d
Natsuneko <10832834+mika-f@users.noreply.github.com>
2021-12-23 23:54:22 +0900

    feat(core): add diagnostic descriptor factory

---
+++ src/Core/DiagnosticCategories.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+namespace NatsunekoLaboratory.UdonAnalyzer;
+
+public static class DiagnosticCategories
+{
+    public const string Compiler = "Compiler";
+    public const string Maintainability = "Maintainability";
+    public const string Security = "Security";
+    public const string Unexpected = "Unexpected";
+    public const string Usage = "Usage";
+}
---
+++ src/Core/DiagnosticDescriptorFactory.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+namespace NatsunekoLaboratory.UdonAnalyzer;
+
+public static class DiagnosticDescriptorFactory
+{
+    private const string HelpLinkBaseUri = "https://docs.natsuneko.moe/udon-analyzer/analyzers/";
+
+    public static DiagnosticDescriptor Create(string id, string title, string messageFormat, string category, DiagnosticSeverity defaultSeverity, bool isEnabledByDefault = true, string? description = null)
+    {
+        return new DiagnosticDescriptor(id, title, messageFormat, category, defaultSeverity, isEnabledByDefault, description, $"{HelpLinkBaseUri}/{id}");
+    }
+}
