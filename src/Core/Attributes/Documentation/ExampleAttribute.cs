7f1967cc76795427cc214c5f6122dd0cd456a7da
Natsuneko <10832834+mika-f@users.noreply.github.com>
2021-12-25 18:48:25 +0900

    feat(core): add documentation attributes

---
+++ src/Core/Attributes/Documentation/ExampleAttribute.cs
@@ -0,0 +1,11 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------------------
+
+using System;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Attributes.Documentation;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ExampleAttribute : Attribute { }
