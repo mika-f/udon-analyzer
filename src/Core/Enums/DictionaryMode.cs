ae818edabedfff906aedbacc001d57438a83b7ae
Natsuneko <10832834+mika-f@users.noreply.github.com>
2021-12-26 16:51:36 +0900

    fix(core): fix header

--- src/Core/Enums/DictionaryMode.cs
+++ src/Core/Enums/DictionaryMode.cs
@@ -1,11 +1,15 @@
-﻿namespace NatsunekoLaboratory.UdonAnalyzer.Enums
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------------------
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Enums;
+
+public enum DictionaryMode
 {
-    public enum DictionaryMode
-    {
-        Direct,
+    Direct,
 
-        Reflection,
+    Reflection,
 
-        Internal
-    }
+    Internal
 }
--- src/Core/Extensions/StringExtension.cs
+++ src/Core/Extensions/StringExtension.cs
@@ -1,7 +1,7 @@
 ﻿// -------------------------------------------------------------------------------------------
 //  Copyright (c) Natsuneko. All rights reserved.
 //  Licensed under the MIT License. See LICENSE in the project root for license information.
-// ------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------
 
 namespace NatsunekoLaboratory.UdonAnalyzer.Extensions;
 
