c44287b0b02c65df35a8ea2269e06172aab8be29
Natsuneko <10832834+mika-f@users.noreply.github.com>
2022-01-17 22:40:25 +0900

    chore(core): add Stay for ConfigureAwait(false)

---
+++ src/Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the Microsoft Reference Source License. See LICENSE in the project root for license information.
+// -----------------------------------------------------------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Extensions;
+
+public static class TaskExtensions
+{
+    public static ConfiguredTaskAwaitable Stay(this Task obj)
+    {
+        return obj.ConfigureAwait(false);
+    }
+
+    public static ConfiguredTaskAwaitable<T> Stay<T>(this Task<T> obj)
+    {
+        return obj.ConfigureAwait(false);
+    }
+}
