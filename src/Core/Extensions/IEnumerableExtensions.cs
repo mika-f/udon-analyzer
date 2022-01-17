5f012c225997354c128e4caa1fc86cc1071cd016
Natsuneko <10832834+mika-f@users.noreply.github.com>
2022-01-17 22:40:58 +0900

    feat(core): add some LINQ extensions

---
+++ src/Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Extensions;
+
+// ReSharper disable once InconsistentNaming
+public static class IEnumerableExtensions
+{
+    public static bool None<TSource>(this IEnumerable<TSource> obj)
+    {
+        return !obj.Any();
+    }
+
+    public static bool None<TSource>(this IEnumerable<TSource> obj, Func<TSource, bool> predicate)
+    {
+        return !obj.Any(predicate);
+    }
+
+    public static IEnumerable<TSource> NotNull<TSource>(this IEnumerable<TSource?> obj) where TSource : class
+    {
+        return obj.Where(w => w != null).Select(w => w!);
+    }
+}
