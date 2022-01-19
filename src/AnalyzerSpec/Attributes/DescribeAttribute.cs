b10167eb5d5171884f97975d00fde22cb8700e5a
Natsuneko <10832834+mika-f@users.noreply.github.com>
2022-01-19 22:57:50 +0900

    feat(analyzer-spec): add spec attributes

---
+++ src/AnalyzerSpec/Attributes/DescribeAttribute.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the Microsoft Reference Source License. See LICENSE in the project root for license information.
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.AnalyzerSpec.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+[DebuggerDisplay("Describe(Analyzer={Analyzer}, Category={Category})")]
+public class DescribeAttribute : Attribute
+{
+    public Type Analyzer { get; }
+
+    public string Category { get; }
+
+    public DescribeAttribute(Type analyzer, string category)
+    {
+        Analyzer = analyzer;
+        Category = category;
+    }
+}
---
+++ src/AnalyzerSpec/Attributes/ExampleAttribute.cs
@@ -0,0 +1,11 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the Microsoft Reference Source License. See LICENSE in the project root for license information.
+// -----------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.AnalyzerSpec.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ExampleAttribute : Attribute { }
