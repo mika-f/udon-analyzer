973a196f9826b5838dfc7130c203132f59b2d35b
Natsuneko <me@natsuneko.cat>
2022-01-24 22:40:22 +0900

    fix(console-core): cleanup code

--- src/ConsoleCore/Helpers/EntityBuilder.cs
+++ src/ConsoleCore/Helpers/EntityBuilder.cs
@@ -5,8 +5,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 using NatsunekoLaboratory.UdonAnalyzer.ConsoleCore.Interfaces;
 
@@ -17,21 +15,6 @@ internal static class EntityBuilder
     public static object? CreateInstance(Type t)
     {
         return Activator.CreateInstance(t);
-    }
-
-    public static void AssignObject(object instance, string key, object value, Type? t = null)
-    {
-        var obj = instance.GetType();
-        var setter = obj.GetProperty(key, BindingFlags.Public)?.SetMethod;
-        if (setter == null)
-            throw new InvalidOperationException($"The property {key} does not have public setter for assigning the parsed value");
-
-        t ??= setter.GetParameters().First().ParameterType;
-
-        if (setter.GetParameters().First().ParameterType != t)
-            throw new InvalidOperationException($"The property {key} does not have the type {t.FullName} for assigning the parsed value");
-
-        setter.Invoke(instance, new[] { value });
     }
 
     public static bool ValidateObject(object instance, out IReadOnlyCollection<IErrorMessage> errors)
