44f234b68415ad2c78b14a69e92e2cd843832aad
Natsuneko <me@natsuneko.cat>
2022-01-24 22:34:48 +0900

    feat(console-core): add helper classes

---
+++ src/ConsoleCore/Helpers/Contract.cs
@@ -0,0 +1,39 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.ConsoleCore.Helpers;
+
+public static class Contract
+{
+    [Conditional("DEBUG")]
+    public static void Assert(bool assertion, string error)
+    {
+        if (assertion)
+            return;
+
+        throw new ArgumentException($"AssertionFailed: {error}");
+    }
+
+    [Conditional("DEBUG")]
+    public static void Require(bool assertion, string error)
+    {
+        if (assertion)
+            return;
+
+        throw new ArgumentException($"RequirementFailed: {error}");
+    }
+
+    [Conditional("DEBUG")]
+    public static void Ensuring(bool assertion, string error)
+    {
+        if (assertion)
+            return;
+
+        throw new ArgumentException($"EnsuringFailed: {error}");
+    }
+}
