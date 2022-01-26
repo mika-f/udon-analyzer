41efc890e1204386e3ccc07de50a1de5fcca8c5e
Natsuneko <me@natsuneko.cat>
2022-01-26 22:31:46 +0900

    feat(core): add safety stack

---
+++ src/Core/Models/SafeStack.cs
@@ -0,0 +1,21 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Models;
+
+public class SafeStack<T> : Stack<T>
+{
+    public bool CanPop()
+    {
+        return Count > 0;
+    }
+
+    public T? SafePeek()
+    {
+        return Count > 0 ? Peek() : default;
+    }
+}
