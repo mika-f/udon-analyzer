76b14c7bb74e41c3bf04555543ce0aaf4f70167a
Natsune <me@natsuneko.cat>
2022-01-30 22:20:37 +0900

    feat(analyzers): support try-catch-finally - VRC0001

---
+++ src/Analyzers/Internal/DiagnosticDescriptors.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Internal;
+
+internal static class DiagnosticDescriptors
+{
+    public static readonly DiagnosticDescriptor TryCatchFinallyIsNotSupported = DiagnosticDescriptorFactory.Create(
+        "VRC0001",
+        "Try/Catch/Finally is not supported by UdonSharp since Udon does not have a way to handle exceptions",
+        "Try/Catch/Finally is not supported by UdonSharp since Udon does not have a way to handle exceptions",
+        DiagnosticCategories.Compiler,
+        DiagnosticSeverity.Error
+    );
+
+}
---
+++ src/Core/DiagnosticHelper.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// -------------------------------------------------------------------------------------------
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace NatsunekoLaboratory.UdonAnalyzer;
+
+public static class DiagnosticHelper
+{
+    public static void ReportDiagnostic(SyntaxNodeAnalysisContext context, DiagnosticDescriptor descriptor, SyntaxNode node, params object[] messageArgs)
+    {
+        ReportDiagnostic(context, Diagnostic.Create(descriptor, node.GetLocation(), messageArgs));
+    }
+
+    public static void ReportDiagnostic(SyntaxNodeAnalysisContext context, Diagnostic diagnostic)
+    {
+        context.ReportDiagnostic(diagnostic);
+    }
+}
