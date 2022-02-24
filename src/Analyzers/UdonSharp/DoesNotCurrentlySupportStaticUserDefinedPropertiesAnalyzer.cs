115540d28c8690fbc69597858deddfd931cc82fb
Natsune <me@natsuneko.cat>
2022-02-24 18:35:52 +0900

    style(analyzers): reformat

--- src/Analyzers/UdonSharp/DoesNotCurrentlySupportStaticUserDefinedPropertiesAnalyzer.cs
+++ src/Analyzers/UdonSharp/DoesNotCurrentlySupportStaticUserDefinedPropertiesAnalyzer.cs
@@ -1,7 +1,7 @@
-//  ------------------------------------------------------------------------------------------
-//   Copyright (c) Natsuneko. All rights reserved.
-//   Licensed under the MIT License. See LICENSE in the project root for license information.
-//  ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -12,24 +12,26 @@ using NatsunekoLaboratory.UdonAnalyzer.Attributes;
 using NatsunekoLaboratory.UdonAnalyzer.Extensions;
 using NatsunekoLaboratory.UdonAnalyzer.Internal;
 
-namespace NatsunekoLaboratory.UdonAnalyzer.UdonSharp
+namespace NatsunekoLaboratory.UdonAnalyzer.UdonSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+[RequireUdonVersion("[2021.11.24.16.19,)")]
+[RequireUdonSharpCompilerVersion("[0.20.3,)")]
+public class DoesNotCurrentlySupportStaticUserDefinedPropertiesAnalyzer : BaseDiagnosticAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp), RequireUdonVersion("[2021.11.24.16.19,)"), RequireUdonSharpCompilerVersion("[0.20.3,)")]
-    public class DoesNotCurrentlySupportStaticUserDefinedPropertiesAnalyzer : BaseDiagnosticAnalyzer
+    public override DiagnosticDescriptor SupportedDiagnostic => DiagnosticDescriptors.DoesNotCurrentlySupportStaticUserDefinedProperties;
+
+    public override void Initialize(AnalysisContext context)
     {
-        public override DiagnosticDescriptor SupportedDiagnostic => DiagnosticDescriptors.DoesNotCurrentlySupportStaticUserDefinedProperties;
-        public override void Initialize(AnalysisContext context)
-        {
-            base.Initialize(context);
+        base.Initialize(context);
 
-            context.RegisterSyntaxNodeAction(w => RunAnalyzer(w, true, AnalyzePropertyDeclaration), SyntaxKind.PropertyDeclaration);
-        }
+        context.RegisterSyntaxNodeAction(w => RunAnalyzer(w, true, AnalyzePropertyDeclaration), SyntaxKind.PropertyDeclaration);
+    }
 
-        private void AnalyzePropertyDeclaration(SyntaxNodeAnalysisContext context)
-        {
-            var declaration = (PropertyDeclarationSyntax)context.Node;
-            if (declaration.HasModifiers(SyntaxKind.StaticKeyword))
-                DiagnosticHelper.ReportDiagnostic(context, SupportedDiagnostic, declaration);
-        }
+    private void AnalyzePropertyDeclaration(SyntaxNodeAnalysisContext context)
+    {
+        var declaration = (PropertyDeclarationSyntax)context.Node;
+        if (declaration.HasModifiers(SyntaxKind.StaticKeyword))
+            DiagnosticHelper.ReportDiagnostic(context, SupportedDiagnostic, declaration);
     }
 }
