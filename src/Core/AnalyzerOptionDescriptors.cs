19274a91f0630c368c15eb5daac0b7ab66c43948
Natsune Mochizuki <me@natsuneko.cat>
2022-07-22 19:30:10 +0900

    fix: fix property name

--- src/Core/AnalyzerOptionDescriptors.cs
+++ src/Core/AnalyzerOptionDescriptors.cs
@@ -11,7 +11,7 @@ public static class AnalyzerOptionDescriptors
 {
     public static readonly AnalyzerOptionDescriptor<bool> EnableWorkspaceAnalyzing = new("udon_analyzer.enable_workspace_analyzing", false);
     public static readonly AnalyzerOptionDescriptor<string> BehaviourInheritFrom = new("udon_analyzer.inherit_from", "UdonSharp.UdonSharpBehaviour");
-    public static readonly AnalyzerOptionDescriptor<string> UdonVirtualMachineVersion = new("udon_analyzer.vrchat_sdk", "auto");
+    public static readonly AnalyzerOptionDescriptor<string> UdonVirtualMachineVersion = new("udon_analyzer.virtual_machine", "auto");
     public static readonly AnalyzerOptionDescriptor<string> UdonSharpCompilerVersion = new("udon_analyzer.udon_sharp", "auto");
     public static readonly AnalyzerOptionDescriptor<string> CSharpLanguageFeature = new("udon_analyzer.csharp_version", "7.3");
     public static readonly AnalyzerOptionDescriptor<string> UdonSharpCompilerIgnoringPreprocessor = new("udon_analyzer.udon_sharp_ignore", "COMPILER_UDONSHARP");
