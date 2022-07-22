cf4f544e046e1071a79fa07c55f5600f534682a6
Natsune Mochizuki <me@natsuneko.cat>
2022-07-22 19:15:43 +0900

    fix: fix default C# version to 7.3

--- src/Core/AnalyzerOptionDescriptors.cs
+++ src/Core/AnalyzerOptionDescriptors.cs
@@ -13,7 +13,7 @@ public static class AnalyzerOptionDescriptors
     public static readonly AnalyzerOptionDescriptor<string> BehaviourInheritFrom = new("udon_analyzer.inherit_from", "UdonSharp.UdonSharpBehaviour");
     public static readonly AnalyzerOptionDescriptor<string> UdonVirtualMachineVersion = new("udon_analyzer.vrchat_sdk", "auto");
     public static readonly AnalyzerOptionDescriptor<string> UdonSharpCompilerVersion = new("udon_analyzer.udon_sharp", "auto");
-    public static readonly AnalyzerOptionDescriptor<string> CSharpLanguageFeature = new("udon_analyzer.csharp_version", "7.2");
+    public static readonly AnalyzerOptionDescriptor<string> CSharpLanguageFeature = new("udon_analyzer.csharp_version", "7.3");
     public static readonly AnalyzerOptionDescriptor<string> UdonSharpCompilerIgnoringPreprocessor = new("udon_analyzer.udon_sharp_ignore", "COMPILER_UDONSHARP");
     public static readonly AnalyzerOptionDescriptor<DictionaryMode> UdonApiDictionaryMode = new("udon_analyzer.api_dictionary_mode", DictionaryMode.Direct);
     public static readonly AnalyzerOptionDescriptor<DictionaryMode> UdonNetworkDictionaryMode = new("udon_analyzer.network_dictionary_mode", DictionaryMode.Direct);
