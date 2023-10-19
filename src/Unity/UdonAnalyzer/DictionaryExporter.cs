4720d4edd96cfbb31ab256484c896d3450d61889
Natsune Mochizuki <me@natsuneko.cat>
2023-10-19 23:26:29 +0900

    chore: add unity integration

---
+++ src/Unity/UdonAnalyzer/DictionaryExporter.cs
@@ -0,0 +1,82 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using UnityEditor;
+
+using UnityEngine;
+
+using VRC.Udon.Editor;
+using VRC.Udon.EditorBindings;
+
+namespace NatsunekoLaboratory.UdonAnalyzer
+{
+    public static class DictionaryExporter
+    {
+        [InitializeOnLoadMethod]
+        public static void OnInitialize()
+        {
+            UdonEditorManager.Instance.GetNodeRegistries();
+
+            var nodeDefinitions = UdonEditorManager.Instance.GetNodeDefinitions().Select(w => w.fullName).ToList();
+            var builtinEvents = new Dictionary<string, string>();
+
+            var interfaces = new UdonEditorInterface();
+
+            foreach (var definition in interfaces.GetNodeDefinitions("Event_"))
+            {
+                if (definition.fullName == "Event_Custom")
+                    continue;
+
+                var str = definition.fullName.Substring("Event_".Length);
+                var name = str.ToCharArray().Select((w, i) => i == 0 ? char.ToLowerInvariant(w) : w);
+
+                if (!builtinEvents.ContainsKey(str))
+                {
+                    var lowercased = string.Join("", name);
+                    builtinEvents.Add(str, "_" + lowercased);
+                }
+            }
+
+            var sb = new StringBuilder();
+            foreach (var definition in nodeDefinitions)
+                sb.AppendLine(definition);
+
+            var assemblies = AssetDatabase.FindAssets("t:asmdef");
+
+            foreach (var assembly in assemblies.Select(AssetDatabase.GUIDToAssetPath))
+            {
+                if (!assembly.StartsWith("Assets"))
+                    continue;
+
+                var path = Path.GetDirectoryName(assembly);
+                var shipped = Path.Combine(path, "PublicAPI.Shipped.txt");
+                using (var sw = new StreamWriter(shipped))
+                    sw.WriteLine(sb.ToString());
+
+                AssetDatabase.Refresh();
+
+                var obj = AssetDatabase.LoadAssetAtPath<TextAsset>(shipped);
+                AssetDatabase.SetLabels(obj, new[] { "RoslynAdditionalFiles" });
+            }
+
+            // for Assembly-CSharp.csproj
+            {
+                var shipped = Path.Combine("Assets", "PublicAPI.Shipped.txt");
+                using (var swr = new StreamWriter(shipped))
+                    swr.WriteLine(sb.ToString());
+
+                AssetDatabase.Refresh();
+
+                var obj = AssetDatabase.LoadAssetAtPath<TextAsset>(shipped);
+                AssetDatabase.SetLabels(obj, new[] { "RoslynAdditionalFiles" });
+            }
+        }
+    }
+}
---
+++ src/Unity/UdonAnalyzer/Hooks/SolutionGeneratorHook.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+using UnityEditor;
+
+namespace NatsunekoLaboratory.UdonAnalyzer.Hooks
+{
+    public class SolutionGeneratorHook : AssetPostprocessor
+    {
+        public static string OnGeneratedCSProject(string path, string content)
+        {
+            var document = XDocument.Parse(content);
+            var @namespace = (XNamespace)"http://schemas.microsoft.com/developer/msbuild/2003";
+            var project = document.Element(@namespace + "Project");
+            var itemGroup = new XElement(@namespace + "ItemGroup");
+
+            {
+                var analyzers = AssetDatabase.FindAssets("l:RoslynAnalyzer").Select(AssetDatabase.GUIDToAssetPath).ToArray();
+                foreach (var analyzer in analyzers)
+                {
+                    var include = new XAttribute("Include", analyzer);
+                    var reference = new XElement(@namespace + "Analyzer", include);
+
+                    itemGroup.Add(reference);
+                }
+            }
+
+            {
+                var additionalFiles = AssetDatabase.FindAssets("l:RoslynAdditionalFiles")
+                                                   .Select(AssetDatabase.GUIDToAssetPath)
+                                                   .Select(w => w.Replace("/", "\\"))
+                                                   .ToArray();
+
+                var items = project.Descendants(@namespace + "ItemGroup")
+                                   .SelectMany(w => w.Descendants(@namespace + "None"))
+                                   .Where(w => additionalFiles.Contains((string)w.Attribute("Include")))
+                                   .ToArray();
+
+                foreach (var item in items)
+                {
+                    var attr = new XElement(@namespace + "AdditionalFiles", new XAttribute("Include", (string)item.Attribute("Include")));
+                    item.ReplaceWith(attr);
+                }
+            }
+
+            project?.Add(itemGroup);
+
+            return document.ToString();
+        }
+    }
+}
