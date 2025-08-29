a5ea07b41d8c97f3f867e7e0ccf70476dff4548b
MOCHIZUKI Natsune <me@natsuneko.cat>
2025-08-29 19:08:49 +0900

    test: add test codefixes for VSC0001

---
+++ src/Tests/Analyzers.Tests/UdonSharp/VSC0001_UdonSharpBehaviourClassesMustBeSameNameAsCsharpFileCodeFixProviderTest.cs
@@ -0,0 +1,57 @@
+﻿// ------------------------------------------------------------------------------------------
+//  Copyright (c) Natsuneko. All rights reserved.
+//  Licensed under the MIT License. See LICENSE in the project root for license information.
+// ------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+using NatsunekoLaboratory.UdonAnalyzer.AnalyzerSpec.Attributes;
+using NatsunekoLaboratory.UdonAnalyzer.Testing;
+using NatsunekoLaboratory.UdonAnalyzer.UdonSharp;
+
+using Xunit;
+
+namespace Analyzers.Tests.UdonSharp;
+
+[Describe(typeof(UdonSharpBehaviourClassesMustBeSameNameAsCsharpFileCodeFixProvider), "VSC")]
+public class UdonSharpBehaviourClassesMustBeSameNameAsCsharpFileCodeFixProviderTest : UdonSharpCodeFixVerifier<UdonSharpBehaviourClassesMustBeSameNameAsCsharpFileAnalyzer, UdonSharpBehaviourClassesMustBeSameNameAsCsharpFileCodeFixProvider>
+{
+    [Fact]
+    [Example]
+    public async Task TestCodeFix_UdonSharpBehaviourClassNameIsDifferentFromCsharpFileName()
+    {
+        await VerifyCodeFixAsync("""
+                                 using UdonSharp;
+
+                                 [|class TestBehaviour2 : UdonSharpBehaviour {}|]
+                                 """,
+                                 """
+                                 using UdonSharp;
+
+                                 class TestSource0 : UdonSharpBehaviour {}
+                                 """
+        );
+    }
+
+    [Fact]
+    public async Task TestCodeFix_UdonSharpBehaviourClassNameIsDifferentFromCsharpFileNameWithReference()
+    {
+        await VerifyCodeFixAsync("""
+                                 using UdonSharp;
+
+                                 [|class TestBehaviour2 : UdonSharpBehaviour
+                                 {
+                                    private TestBehaviour2 _ref;
+                                 }|]
+                                 """,
+                                 """
+                                 using UdonSharp;
+
+                                 class TestSource0 : UdonSharpBehaviour
+                                 {
+                                    private TestSource0 _ref;
+                                 }
+                                 """
+        );
+    }
+}
